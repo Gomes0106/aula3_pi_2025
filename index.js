4acf882437aa6520f8088febe392d65bb58769d3
GUILHERME GOMES SILVA <11625086636@pedag.local>
2025-08-21 15:13:47 -0300

    testeh

--- index.js
+++ index.js
@@ -3,21 +3,13 @@ const mongoose = require('mongoose');
 const port = 3000;
 const dotenv = require('dotenv');
 const app = express();
+const conectaDB = require('./db');
 
 app.use(express.json())
 
 dotenv.config();
 
-const uri = process.env.MONGODB_URI;
-
-mongoose.connect(uri)
-   .then(() => {
-       console.log(`Conectando ao mongo Atlas`);
-   })
-   .catch((err) =>{
-       console.error(`Erro ao conectar ${err}`);
-   });
-
+conectaDB();
 
 app.get('/', (req,res) =>{
     res.json({message: "Hello World"});
@@ -27,3 +19,4 @@ app.listen(port, () => {
     console.log(`Servidos rodando na porta ${port}`);
 })
 
+//aaaaa
